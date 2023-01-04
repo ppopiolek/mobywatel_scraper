6246e5c10712f32dfb58da028dd65943465136e3
Mateusz Borkowski <56604997+Matie26@users.noreply.github.com>
2023-01-04 21:56:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
-apt-get -y update
+apt update -y
 
-apt-get install -y google-chrome-stable
+apt install -y google-chrome-stable
 
-apt-get install -yqq unzip
+apt install -yqq unzip
 
-apt-get install -y python3-pip
+apt install -y python3-pip
 
 wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
 
