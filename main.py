4c027d6c17b64acdbae5c8b0f8541e633aca296e
Paweł Popiołek <56610497+ppopiolek@users.noreply.github.com>
2023-01-08 14:52:04 +0100

    Update main.py
    added pretty table creation and pdf saving

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import json
+ 
 def get_element(driver, css_selector):
     return  WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.CSS_SELECTOR, css_selector))
-
+ 
 css_selectors = {
     "profil_zaufany": ("button.icon-button:nth-child(2)", {
         "mail": ".data-list > div:nth-child(1) > span:nth-child(1)",
@@ -52,13 +57,13 @@ css_selectors = {
         "data_zameldowania_na_pobyt_staly": "div.ng-star-inserted:nth-child(9) > span:nth-child(2)"
     })
 }
-
+ 
 if __name__ == "__main__":
     driver = webdriver.Firefox()
     driver.get("https://serwis.epuap.gov.pl/mlpz/login?ORIGIN=nforms_EServices")
     WebDriverWait(driver, 180).until(lambda driver: driver.current_url == "https://www.mobywatel.gov.pl/mObywatel")
     driver.get("https://www.mobywatel.gov.pl/mObywatel/twoje-dane")
-
+ 
     data = {}
     for section in css_selectors:
         button = get_element(driver, css_selectors[section][0])
@@ -69,4 +74,25 @@ if __name__ == "__main__":
             except Exception as e:
                 print(e)
                 data[field] = 'error'
-    print(data)
+                
+    #make pretty string from dict and output as txt
+    data_string = json.dumps(data, indent = 4, sort_keys = True)
+    text_file = open('dane.txt', 'w')
+    text_file.write(data_string)
+    text_file.close()
+    
+    #make df from dict
+    df = pd.DataFrame.from_dict(data, orient = 'index', columns = ['WARTOSC'])
+    df.reset_index(inplace = True)
+    df.rename(columns = {'index':'POLE'}, inplace = True)
+    
+    #make matplotlib table from df
+    fig, ax = plt.subplots(figsize = (12,4))
+    ax.axis('tight')
+    ax.axis('off')
+    the_table = ax.table(cellText = df.values, colLabels = df.columns, loc = 'center', cellLoc = 'left', colColours =    ['moccasin', 'moccasin'], colWidths = [0.33, 0.33])
+    
+    #save table as pdf
+    pp = PdfPages('dane.pdf')
+    pp.savefig(fig, bbox_inches = 'tight')
+    pp.close()
